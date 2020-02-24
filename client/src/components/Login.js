b9039f5b1a0d14cdbf249cb66ed73b3853d12bab
mikejeter <mjeter45@yahoo.com>
2020-02-23 21:27:45 -0500

    Created PrivateRoute.js, Modified Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: 200
+    }
+  }
+}));
+
+const Login = props => {
+  const classes = useStyles();
+  const [data, setData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = event => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", data)
+      .then(response => {
+        window.localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(error => console.log(error));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div className="formContainer">
+        <h1>Welcome to the Bubble App!</h1>
+        <h3>Sign In</h3>
+        <form
+          onSubmit={handleSubmit}
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              id="outlined-basic"
+              onChange={handleChange}
+              value={data.username}
+              type="text"
+              name="username"
+              label="Username"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-basic"
+              onChange={handleChange}
+              value={data.password}
+              type="password"
+              name="password"
+              label="Password"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
   );
 };
 
